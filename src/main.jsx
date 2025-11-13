846dc46c4f7ec8259575887862a1c9f0048d2712
MateeGonzaa <mateo.gonzalez@itecriocuarto.org.ar>
2025-11-13 02:12:39 -0300

    actualizo main.jsx y paquete json

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+// Importaciones de PrimeReact
+// 1. Tema
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+// 2. Core PrimeReact
+import "primereact/resources/primereact.min.css";     
+// 3. Iconos
+import "primeicons/primeicons.css";                                 
+// 4. Utilidades de Flexbox (para responsive)
+import "primeflex/primeflex.css";                                   
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
